ab3239de425e7f66f0799c448b0bce8dc10137ac
Blaire Baker <bakerblaire@gmail.com>
2020-11-04 23:42:21 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   random_number = rand(6) + 1
   input = gets.chomp
-  if input == random_number
+  if input.to_i == random_number
     puts "You guessed the correct number!"
   elsif input == "exit"
     puts "Goodbye!"
