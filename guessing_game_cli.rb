b592c8e0a82011726c1612fcff08606a5a1ccfad
Blaire Baker <bakerblaire@gmail.com>
2020-11-04 23:32:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,4 +2,11 @@ run_guessing_game
   random_number = rand(6) + 1
   puts "Pick a number between 1 and 6"
   input = gets.chomp
+  if input == random_number
+    puts "You guessed the correct number!"
+  elsif input == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{random_number}"
+  end
 end
